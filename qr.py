90dfd8ebc10325c3d3de30a9febc1f6ef6bfa074
tdanny00 <tolgyesipontdani@gmail.com>
2022-10-27 08:15:52 +0200

    .

--- qr.py
+++ qr.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread('qrcode.png')
+image = cv2.imread('qr3.png')
 
 image = cv2.resize(image, (1000, 1000))
 
@@ -10,13 +10,14 @@ edges = cv2.Canny(gray, 100, 200)
 
 contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-goodContours = []
+goodContours =[]
 for c in contours:
     approx = cv2.approxPolyDP(c, 1, True)
-    print(approx)
+
+if len(approx) == 4:
     goodContours.append(approx)
 
-image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2, cv2.LINE_AA)
-
+image = cv2.drawContours(image, goodContours, -1, (0, 255, 0), 2, cv2.LINE_AA)
+print(goodContours)
 cv2.imshow("kep", image)
 cv2.waitKey(0)
