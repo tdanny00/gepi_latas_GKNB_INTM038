ac5fa91a1571f40a2a0f2f4a1f37744d82bafa8d
tdanny00 <tolgyesipontdani@gmail.com>
2022-10-27 12:19:05 +0200

    recognition method with contour hierarchy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gepi_latas_GKNB_INTM038
+
+Dataset for testing: https://drive.google.com/drive/folders/1db8rTwYgnIpVViH3mrGVc7GJ7q630_G5
--- qr.py
+++ qr.py
@@ -1,23 +1,52 @@
 import cv2
+import glob
+import numpy as np
 
-image = cv2.imread('qr3.png')
+kernel = np.array([[0, -1, 0],
+                   [-1, 5,-1],
+                   [0, -1, 0]])
+vid = cv2.VideoCapture(0)
+def qrRecognizer(name, inputImage):
+    resizedImage = cv2.resize(inputImage, (600, 600))
+    grayScaledImage = cv2.cvtColor(resizedImage, cv2.COLOR_BGR2GRAY)
+    edgesWithCanny = cv2.Canny(grayScaledImage, 150, 250)
+    contours, hierarchy = cv2.findContours(edgesWithCanny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-image = cv2.resize(image, (1000, 1000))
+    hierarchy = hierarchy[0]
+    neededContours = []
+    def getDepth(currentHierarchy):
+        depth = 0
+        while currentHierarchy[2] > 0:
+            depth += 1
+            currentHierarchy = hierarchy[currentHierarchy[2]]
+        return depth
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    for i,c in enumerate(contours):
+        if getDepth(hierarchy[i]) >= 5:
+            neededContours.append(c)
 
-edges = cv2.Canny(gray, 100, 200)
 
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    drawedImage = cv2.drawContours(resizedImage, neededContours, -1,(83, 255, 0), 2)
+    cv2.imshow(name, drawedImage)
+    if len(neededContours) >= 3:
+        print("Qr code Founded!")
+    else:
+        print("Qr code not Founded!")
+    
+  #Implemented and working with videocapture too.
 
-goodContours =[]
-for c in contours:
-    approx = cv2.approxPolyDP(c, 1, True)
+#while(True):
+ #   ret, frame = vid.read()
+  #  newFrame = cv2.filter2D(frame, -1, kernel)
+   # qrRecognizer("frame", newFrame)
+    
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+     #   break
+  
+#vid.release()
+#cv2.destroyAllWindows()
 
-if len(approx) == 4:
-    goodContours.append(approx)
-
-image = cv2.drawContours(image, goodContours, -1, (0, 255, 0), 2, cv2.LINE_AA)
-print(goodContours)
-cv2.imshow("kep", image)
+images = [cv2.imread(file) for file in glob.glob("./samples/*")]
+for i,image in enumerate(images):
+    qrRecognizer("Image %d" % (i),image)
 cv2.waitKey(0)
